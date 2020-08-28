1eeccb92079dd3df928f139eae6f23020c49d38a
Harvinder Singh <46685955+Harvinder-osahan@users.noreply.github.com>
2020-08-29 03:38:47 +0530

    Update job2.groovy

--- job2.groovy
+++ job2.groovy
@@ -10,30 +10,30 @@ shell('''sudo cd /root/task6/
       ''')        
 shell('''if ! kubectl get pvc | grep pvc
          then
-          kubectl create -f /root/.jenkins/workspace/seed_job/pvc.yml
+          kubectl create -f /root/.jenkins/workspace/Code-Fetch/pvc.yml
          fi
-         if ls /root/.jenkins/workspace/seed_job/ | grep .php
+         if ls /root/.jenkins/workspace/Code-Fetch/ | grep .php
          then
           if kubectl get deploy | grep php
           then 
            PODS=$(kubectl get pods -l app=php -o jsonpath="{.items[*].metadata.name}")
            for i in $PODS
             do 
-             kubectl cp /root/.jenkins/workspace/seed_job/*.php $i:/var/www/html/
+             kubectl cp /root/.jenkins/workspace/Code-Fetch/*.php $i:/var/www/html/
             done
           else
-           kubectl  create -f /root/.jenkins/workspace/seed_job/deploy.yml
+           kubectl  create -f /root/.jenkins/workspace/Code-Fetch/deploy.yml
            sleep 25
            PODS=$(kubectl get pods -l app=php -o jsonpath="{.items[*].metadata.name}")
            for i in $PODS
             do 
-             kubectl cp /root/.jenkins/workspace/seed_job/*.php $i:/var/www/html/
+             kubectl cp /root/.jenkins/workspace/Code-Fetch/*.php $i:/var/www/html/
             done
           fi 
          fi  
          if ! kubectl get svc | grep svc
          then
-          kubectl create -f /root/.jenkins/workspace/seed_job/svc.yml
+          kubectl create -f /root/.jenkins/workspace/Code-Fetch/svc.yml
          fi''') 
 }
 } 
